7942aee7e032193486b2e17430d59db5e0e2362d
JohnCook17 <655@holbertonschool.com>
2019-09-09 17:10:26 -0700

    task 5, need to make count work

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 @app_views.route("/status")
 def app_status():
-    return jsonify('"status": "OK"')
+    return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def cool_stats():
+    return jsonify({
+        "amenities": storage.count("amenities"),
+        "cities": storage.count("cities"),
+        "places": storage.count("places"),
+        "reviews": storage.count("reviews"),
+        "states": storage.count("states"),
+        "users": storage.count("users")
+    })
