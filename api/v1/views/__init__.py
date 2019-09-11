51c37e5db8b310d0e5f6868ec290911fd71f89c0
renefibonacci660 <660@holbertonschool.com>
2019-09-11 18:30:51 +0000

    Added documentation to init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Init file that is importing blueprint to create instances as needed """
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
