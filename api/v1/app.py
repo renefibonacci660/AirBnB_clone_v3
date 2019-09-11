cdc54f28db60ff9a5f9c6d3c0a5656235c92b619
JohnCook17 <655@holbertonschool.com>
2019-09-11 10:22:32 -0700

    added getenv

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -15,4 +16,4 @@ def not_found(var):
     return jsonify({"error": "Not found"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST") or "0.0.0.0", port=getenv("HBNB_API_PORT") or 5000, threaded=True)
