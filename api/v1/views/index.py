a3f55fb06bac77c161b015effbc899dcc62cc1cf
renefibonacci660 <660@holbertonschool.com>
2019-09-11 18:27:26 +0000

    Added documentation to file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Provides status of our Flask instance route of 'app' """
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
@@ -6,11 +7,13 @@ from flask import jsonify
 
 @app_views.route("/status")
 def app_status():
-    return jsonify({ "status": "OK" })
+    """ App status if successful """
+    return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats")
 def cool_stats():
+    """ Endpoint that retrieves the number of each objects by type """
     return jsonify({
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
