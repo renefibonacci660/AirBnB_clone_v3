a066594cdd2c92b34e88d6ca1527c7a025fb5016
renefibonacci660 <660@holbertonschool.com>
2019-09-12 20:41:02 +0000

    Moving name instance before called in if statement in POST method

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -39,12 +39,11 @@ def delete_amenities(amenity_id):
 def post_amenities():
     """ Creates a Amenity """
     content = request.get_json()
+    name = content.get("name")
     if name is None:
         return (jsonify({"error": "Missing name"}), 400)
     if content is None:
         return (jsonify({"error": "Not a JSON"}), 400)
-
-    name = content.get("name")
     new_amenity = Amenity(**content)
     new_amenity.save()
 
